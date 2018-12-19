bd44dad6de426ddf50874c1c4378f428079555c5
Bruno Ploumhans <bruno.ploumhans@gmail.com>
2018-12-20 00:00:09 +0100

    Code inlining and formatting

--- src/constant.rs
+++ src/constant.rs
@@ -1,9 +1,8 @@
-pub const RADIUS : f64 = 0.9;
-pub const NORMAL : (f64, f64, f64) =  (0.9, 0.9, 0.4);
-pub const D : f64 = 4.0;
-pub const EPS : f64 = 1e-4;
-pub const H : f64 = 0.025;
+pub const RADIUS: f64 = 0.9;
+pub const NORMAL: (f64, f64, f64) = (0.9, 0.9, 0.4);
+pub const D: f64 = 4.0;
+pub const EPS: f64 = 1e-4;
+pub const H: f64 = 0.025;
 pub const A: f64 = 5.0;
-pub const SEED: i32 = 1;
 pub const BIOME_MAP_SIZE: usize = 512;
-pub use std::f64::consts::PI as PI;
+pub use std::f64::consts::PI;
